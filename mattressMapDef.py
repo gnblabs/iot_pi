61066431708501e251a37eb87021f69ae7b2ff75
Goli <bhargavgoli@gmail.com>
2023-06-16 18:33:33 -0500

    formatted print

--- mattressMapDef.py
+++ mattressMapDef.py
@@ -102,7 +102,7 @@ class MattressHeatMap:
                     'x-api-key': self.apiKey
                 }
             )
-            print('data posted successfully!!! '+ responseUloadPM.status_code)
+            print("data posted successfully!!! with status code {}".format(responseUloadPM.status_code))
         except requests.exceptions.ConnectionError as e:
             print('Connection refuse error in request at new server', e)
         return data
