57036b894aee83f1d152b70c3890f8629c5bf643
Goli <bhargavgoli@gmail.com>
2023-06-21 19:28:08 -0500

    max time out

--- poll.sh
+++ poll.sh
@@ -2,7 +2,7 @@
 
 while true; do
   # Make GET request using curl
-  curl --max-time 4 http://localhost:3000/bedMap
+  curl --max-time 15 http://localhost:3000/bedMap
 
   # Sleep for 1 second before making the next request
   sleep 4
