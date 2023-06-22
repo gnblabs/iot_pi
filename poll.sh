9b2f0b29cfe03bec31526b86bdcb4c9fd1751ebb
Goli <bhargavgoli@gmail.com>
2023-06-21 19:24:03 -0500

    timeout fix

--- poll.sh
+++ poll.sh
@@ -2,7 +2,7 @@
 
 while true; do
   # Make GET request using curl
-  curl --connection-timeout 15 -X GET http://localhost:3000/bedMap
+  curl --connection-timeout 15 http://localhost:3000/bedMap
 
   # Sleep for 1 second before making the next request
   sleep 4
